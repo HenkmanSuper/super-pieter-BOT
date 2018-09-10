0968dacaa447b3ee8161a4d4fa03bff9d2ba3e7f
pk88 <42702011+superpieter88@users.noreply.github.com>
2018-09-10 14:48:20 +0200

    Update super_pieter_bot.py

--- super_pieter_bot.py
+++ super_pieter_bot.py
@@ -1,3 +1,23 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
 
 
-client.run("NDg4Mjc1OTQ3NjkxNDQyMTg3.Dnbdgg.K9RVYcxUlBUctPt3rwm2vtmXEBc") 
+Client = discord.Client() #Initialise Client 
+client = commands.Bot(command_prefix = "?") #Initialise client bot
+
+
+@client.event 
+async def on_ready():
+    print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
+
+@client.event
+async def on_message(message):
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:") #responds with Cookie emoji when someone says "cookie"
+
+client.run("NDg4Mjc1OTQ3NjkxNDQyMTg3.DnfxXQ.Ear2_QUHygAA02wTXzIMEVMcK4s")
+
+
